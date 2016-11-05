c5807a515e2d952946713d6fdaa9e6a45b704fda
keeper <keeper.keeper@gmail.com>
2016-11-05 19:32:46 +0300

    Error if VS_API_KEY env variable is not set

--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+[[ -z "$VS_API_KEY" ]] && {
+    echo "Please set Vscale API token via VS_API_KEY env variable"
+	echo "export VS_API_KEY=<your token>"
+    exit 1
+}
+
 ansible-playbook destroy.yml -i hosts
